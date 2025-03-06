68d186243240cc177e575c7e916ea4ee1ad606b2
rickeymolape <rickeymolape@gmx.com>
2025-03-06 16:29:54 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,8 @@
+import java.util.*; 
+
+public class MathProblems{
+ public static void main(String[] args) {
+  int rand = new Random().nextInt(100); 
+  System.out.println("A random number between 1 and 100 is " + rand); 
+ } 
+}
